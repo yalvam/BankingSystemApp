38749c00470f025161f381aa63d07804765f6c0c
yalvam <yashashwini.alva@outlook.com>
2024-07-01 19:07:23 +0100

    test1 change

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace BankingSystemApp
         [STAThread]
         static void Main()
         {
+            //sss//
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
